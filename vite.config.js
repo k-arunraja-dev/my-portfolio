cfd76a4608d195fbbfa60b1e8dee2a59e604389c
k-arunraja-dev <karunraja.dev@gmail.com>
2025-06-26 18:51:03 +0530

    Configured GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
-      },
-    },
-  },
+  base: '/my-portfolio/', // 👈 your repo name here
   plugins: [react()],
-})
+});
